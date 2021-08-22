07de79a52148e1be3725ca5930be8778b80ce746
Caleb Matteis <matteismusic@gmail.com>
2021-08-22 00:59:03 -0400

    commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
 const client = new Client({
-  connectionString: DB_URI,
+  connectionString: process.env.DATBASE_URL || DB_URI,
 });
 
 client.connect();
